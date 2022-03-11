bf4bfd103064417f795b0b9cdecae6cc0b65e3f2
Dhanraj.Raiturkar <Dhanraj.Raiturkar@zapcg.com>
2022-03-11 14:31:38 +0530

    mongoDB connected to backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const connectDB = () => {
+const connectDB = async() => {
     try{
-        const conn = mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
-            useNewUrlParse: true,
+            useNewUrlParser: true,
             useCreateIndex: true,
         });
-
-        console.log(`MongoDB Connected: ${conn}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error){
         console.log(error.message);
         process.exit();
