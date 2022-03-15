aed55d6b9ca86918f0dd90ffcb3668cd73724729
Dhanraj.Raiturkar <Dhanraj.Raiturkar@zapcg.com>
2022-03-15 15:57:14 +0530

    added redux and api for user login/register and notes crud ops

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,41 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
+const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send("api is running...");
 })
 
 // fetch all notes
-app.get('/api/notes', (req,res) => {
-    res.send(notes);
-})
+// app.get('/api/notes', (req,res) => {
+//     res.send(notes);
+// })
 
-app.get('/api/notes/:id', (req,res) => {
-    const found = notes.some(note => parseInt(note._id) === parseInt(req.params.id));
+// app.get('/api/notes/:id', (req,res) => {
+//     const found = notes.some(note => parseInt(note._id) === parseInt(req.params.id));
 
-    if(found){
-        const note = notes.filter(note => note._id === parseInt(req.params.id));
-        res.send(note);
-    }
-    else{
-        res.send(`Note with id ${req.params.id} does not exist`);
-    }
-})
+//     if(found){
+//         const note = notes.filter(note => note._id === parseInt(req.params.id));
+//         res.send(note);
+//     }
+//     else{
+//         res.send(`Note with id ${req.params.id} does not exist`);
+//     }
+// })
+
+app.use('/api/users', userRoutes);
+app.use('/api/notes', noteRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
